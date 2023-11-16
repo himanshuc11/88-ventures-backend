280e1eb9a824a6f9d683b44b2de098d630e2bf00
himanshuc11 <himanshuchhatpar@gmail.com>
2023-11-16 21:53:57 +0530

    init: Cloudinary upload images setup

--- server.ts
+++ server.ts
@@ -1,6 +1,16 @@
+import "dotenv/config";
+import cloudinary from "./cloudinary.js";
 import express, { Application } from "express";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log("Server is Fire @ " + PORT));
+app.listen(PORT, () => {
+  cloudinary.uploader.upload(
+    "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+    { public_id: "olympic_flag" },
+    function (error, result) {
+      console.log(result);
+    }
+  );
+});
